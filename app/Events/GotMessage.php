908d9aee22d5a64501504af1e72a2b280f4eea18
Saleem_Abou_Assi <amhadi696@gmail.com>
2024-07-16 12:42:46 +0300

    chat app

---
+++ app/Events/GotMessage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class GotMessage implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public array $message) {
+        //
+    }
+
+    public function broadcastOn(): array {
+        // $this->message is available here
+        return [
+            new PrivateChannel("channel_for_everyone"),
+        ];
+    }
+}
